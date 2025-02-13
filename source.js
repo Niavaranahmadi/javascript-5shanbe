58a4892a5ab76e7678833b3eddb13ddd60174f7d
Niavaranahmadi <niavaran834@gmail.com>
2025-02-13 13:49:48 +0330

    5shanbe

---
+++ source.js
@@ -0,0 +1,243 @@
+// console.log("Hello world!");
+
+
+// console.log(45+2);
+// console.log(50-10);
+// console.log(7*2);
+// console.log(14/2);
+// console.log(2**2);
+// console.log(10%3);
+
+
+
+// variable
+
+// let a = 50;
+// console.log(a);
+// console.log(a*2);
+
+
+// let a = 10;
+// a = a+5;
+// console.log(a);
+
+
+// var a = 45;
+// a = a-5;
+// console.log(a);
+
+
+// const a = 12;
+// a = a-5;
+// console.log(a);
+
+
+
+// var a = 4;
+// console.log(typeof a);
+
+
+// let a = "hello";
+// console.log(a);
+// console.log(typeof a);
+
+
+// let colors = ["blue" , "green" , "red"];
+// console.log(colors);
+// console.log(typeof colors);
+
+
+// let info = {
+//     name : "ali",
+//     family : "mallah",
+//     age : 22,
+//     job : "doctor"
+// };
+// console.log(info);
+// console.log(info.name);
+// console.log(info.age);
+
+
+
+// Boolean 
+
+// let a = 4>5;
+// console.log(a);
+// console.log(typeof a);
+
+
+// let a = 5>4;
+// console.log(a);
+
+
+
+// index 
+
+// var text = "hello good morning";
+// console.log(text[4]);
+// console.log(text[0]);
+// console.log(text.at(-1));
+// console.log(text.at(2));
+
+// console.log(text[2]);
+// console.log(text.at[2,9]);
+
+// console.log(text.slice(2,4));
+// console.log(text.slice(6));
+
+
+
+// if (6>7){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+
+
+// let a = 4;
+// let b = '4';
+// console.log(a!=b);
+// console.log(a!==b);
+
+// console.log(a==b);
+// console.log(a===b);
+
+
+// let a = 15;
+// let b = '15';
+// if (a==b){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// };
+
+
+// if (a===b){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// };
+
+
+
+
+// let a = "hello";
+// if (a.length>5){
+//     alert("yes");
+// }else{
+//     alert("no");
+// };
+
+
+// let date = new Date();
+// let year = date.getFullYear();
+// console.log(year);
+
+// let month = date.getMonth()+1;
+// console.log(month);
+// let day = date.getDate();
+// console.log(day)
+
+// console.log(`${year}/${month}/${day}`);
+
+
+
+
+// let date = new Date();
+// let hour = date.getHours();
+// let minutes = date.getMinutes();
+// let seconds = date.getSeconds();
+// console.log(`${hour}:${minutes}:${seconds}`);
+
+
+
+// let btn = document.getElementById("btn");
+// let name = document.getElementById("name");
+// let age = document.getElementById("age");
+
+// btn.addEventListener("click" , function(){
+//     if (age.value>=18){
+//         alert(`Welcome ${name.value}`);
+//     }else{
+//         alert(`boro khune`);
+//     }
+// })
+
+
+
+
+// let a = 45;
+// let b = 20;
+// if (a>b && a+b==65){
+//     console.log("hello");
+// }else{
+//     console.log("bye");
+// };
+
+
+// if (a<b || a+b>65){
+//     console.log("hello");
+// }else{
+//     console.log("bye");
+// };
+
+
+
+
+
+
+// let a = "farzad";
+// switch (a){
+//     case "farjad":
+//         console.log("hello");
+//         break
+//     case "farzad":
+//         console.log("bye bye...");
+//         break
+//     default:
+//         console.log("sadfasdfsadfas")
+//         break
+// }
+
+
+
+
+
+
+// let form = document.getElementById("loginform");
+
+// form.addEventListener("submit" , function(event){
+//     event.preventDefault()
+//     let region = document.getElementById("region").value;
+//     switch (region){
+//         case "iran":
+//             alert("welcome");
+//             break
+//         case "russia":
+//             alert("welcome");
+//             break
+//         default:
+//             alert("this site cant reach in your region!!!!");
+//             break
+//     }
+// })
+
+
+
+// let a = 4.8;
+// console.log(Math.round(a));
+
+
+// let a = 4.4;
+// console.log(Math.round(a));
+
+// let a = 4.4;
+// console.log(Math.ceil(a));
+
+
+// let a = 4.7;
+// console.log(Math.floor(a));
+
+
+
